c36250edebc8a1c1e7e269ba69c697a9b95c7321
夏宁 <478084629@qq.com>
2022-01-30 03:38:11 +0800

    代码提交

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,6 +2,8 @@
     <div>
         <div>123211232</div>
         <Model v-model="name" @change='handle' @click="handle1"/>
+        <div style="height:800px">1</div>
+        <div style="height:600px" id="item">2</div>
     </div>
 </template>
 <script>
@@ -12,6 +14,13 @@ export default {
         return{
             name:1
         }
+    },
+    mounted(){
+        console.log('--------',this.$router.currentRoute);
+        console.log(this.$route);
+        this.$nextTick(()=>{
+            document.getElementById('item').scrollIntoView()
+        })
     },
     methods:{
         handle(e){
