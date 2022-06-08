7f1da6d59df92a30b1cdee70ca670ad9c6eb54ad
夏宁 <478084629@qq.com>
2022-06-09 01:30:06 +0800

    1

--- src/views/File.vue
+++ src/views/File.vue
@@ -1,30 +1,60 @@
 <template>
-    <div>
-       <Btn placeholder='shj' @change="handleChange"/>
-       <Edit style="width:400px;" v-model="value" />
+    <div class="file" ref='vantaRef'>
+       <!-- <Btn placeholder='shj' @change="handleChange"/>
+       <Edit style="width:400px;" v-model="value" /> -->
     </div>
 </template>
 <script>
-import Btn from '../components/btn.vue'
+// import Btn from '../components/btn.vue'
 // import Edit from '../components/Edit.vue';
+import * as THREE from 'three'
+import BIRDS from 'vanta/src/vanta.fog'
+// console.log(THREE);
 export default {
-    components:{Btn},
+    // components:{Btn},
     data(){
         return {
-            value:''
+            value:'',
+            vantaEffect: null
         }
     },
-    watch:{
-        value:{
-            handler(val){
-                console.log(val);
-            }
-        }
-    },
-    methods:{
-        handleChange(val){
-            console.log(val);
-        }
+
+  mounted() {
+    this.vantaEffect = BIRDS({
+      el: this.$refs.vantaRef,
+      THREE: THREE,
+    })
+    this.vantaEffect.setOptions({
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00
+    })
+  },
+  beforeDestroy() {
+    if (this.vantaEffect) {
+      this.vantaEffect.destroy()
     }
+    // watch:{
+    //     value:{
+    //         handler(val){
+    //             console.log(val);
+    //         }
+    //     }
+    // },
+    // methods:{
+    //     handleChange(val){
+    //         console.log(val);
+    //     }
+  }
 }
 </script>
+<style scoped>
+ .file{
+     width: 100vw;
+     height: 100vh;
+ }
+</style>
