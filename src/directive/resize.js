b1001ba3cddb77017cf615448e5810b1ca550eba
夏宁 <478084629@qq.com>
2023-05-04 14:54:35 +0800

    1

--- src/directive/resize.js
+++ src/directive/resize.js
@@ -3,15 +3,15 @@
  * @Auther: xianing
  * @LastEditors: xianing
  * @Date: 2023-05-04 14:19:47
- * @LastEditTime: 2023-05-04 14:52:24
+ * @LastEditTime: 2023-05-04 14:53:24
  */
 const map = new WeakMap() // 创建映射关系
 const ob = new ResizeObserver(items => {
     for (const item of items) {
-        // console.log(item);
         const handler = map.get(item.target)
         if (handler) {
             const box = item.borderBoxSize[0]
+            // 直接调用函数
             handler({
                 with: box.inlineSize, // 宽度
                 height: box.blockSize // 高度
