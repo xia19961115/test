bb09d5c09b4ea2eaf36615abb43f67d628a91427
夏宁 <478084629@qq.com>
2023-08-05 01:40:38 +0800

    1

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,10 +1,3 @@
-<!--
- * @Description: 
- * @Auther: xianing
- * @LastEditors: xianing
- * @Date: 2023-08-01 23:30:24
- * @LastEditTime: 2023-08-05 01:35:17
--->
 <template>
-    <div>404-</div>
+    <div>404</div>
 </template>
